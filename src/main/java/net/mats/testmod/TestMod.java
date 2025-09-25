58c9e3f225e8232ef32409898720b9624659d811
Mats1510 <matej.sida05@gmail.com>
2025-09-25 12:54:18 +0200

    Test

--- src/main/java/net/mats/testmod/TestMod.java
+++ src/main/java/net/mats/testmod/TestMod.java
@@ -1,7 +1,11 @@
 package net.mats.testmod;
 
 import com.mojang.logging.LogUtils;
+import net.mats.testmod.Item.ModCreativeModTabs;
+import net.mats.testmod.Item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -22,6 +26,9 @@ public class TestMod {
     public TestMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -39,6 +46,10 @@ public class TestMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
